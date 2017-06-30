17c284e5e3f2e0f2d78381d12cdfb74d15259b86
Tomas <tomasp1189@gmail.com>
2017-06-30 15:42:01 -0300

    Finished onSelectedVideo

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
-
-import SearchBar from './SearchBar'
+import YTSearch from 'youtube-api-search';
+import SearchBar from './SearchBar';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
+const API_KEY = 'AIzaSyDiOUZVX77uEIKvUD0YSxpBnwJCHqIMdkA';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, videos => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      })
+    });
+    // equivalent to this.setState({videos: videos})
+  }
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos} />
       </div>
     );
   }
